bd6efbbda5876352298000e998041373a4433610
Brian Allen <brianallen2401@gmail.com>
2020-02-08 14:22:47 -0700

    all work done in class is saved and up to date

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// this is the card created in jacobs code
+// 
+// ==================================================
+//   <div class="card card mb-4 shadow-sm mx-auto">
+//     <div class="card-header text-wrap">
+//      <h3>
+//      <h6>
+//      <h7>
+//     </div>
+//     <div class="card-body">
+//       <img>
+//     </div>
+//   </div>
+// ===================================================
