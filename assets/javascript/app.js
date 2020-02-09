64e2245f6b6203fdc90ed27080fe6ce34629822f
Derrick Hepworth <derrickhepworth@gmail.com>
2020-02-09 09:46:47 -0700

    CSS updates, linked to database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,45 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAggh_9HPrLN-IokUfsrCz2bCP_4ABUd4Y",
+    authDomain: "salty-beards-recipe-box.firebaseapp.com",
+    databaseURL: "https://salty-beards-recipe-box.firebaseio.com",
+    projectId: "salty-beards-recipe-box",
+    storageBucket: "salty-beards-recipe-box.appspot.com",
+    messagingSenderId: "655365438357",
+    appId: "1:655365438357:web:3b219292065eb7ad0e149c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // this is the card created in jacobs code
 // 
 // ==================================================
