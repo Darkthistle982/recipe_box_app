8949af77bd26858519f29968444a7207c7a0c0b6
Jacob Stanger <jacobcstanger@gmail.com>
2020-02-12 18:10:53 -0700

    dynamic cards yall!!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,34 +10,27 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+
+
 $(document).ready(function() {
 
     // ===================================================
-    // EVENT - save recipe
+    // EVENT - save recipe ( + ) button 
     // ===================================================
     $('#save-recipe-btn').on('click', function(event) {
         console.log('here is event', event);
         var title = $('#recipe-input').val();
         console.log('here is title: ' + title);
 
-        // TODO: save form entry to database
-
-
-        // TODO: when the data is saved to database, add success message
-        var successMessage = $('<div>').addClass('alert alert-success')
-            .attr('role', 'alert')
-            .text('Yay! you saved something!');
-
-        $('.card-message').prepend(successMessage);
-
-        // removes message after 5 seconds  
-        setTimeout(function() {
-            $('.card-message').detach();
-
-        }, 4 * 1000);
-
+        addSuccessMessage('card-message');
 
     });
+
+    // $('#addNewRecipe').on('shown.bs.modal', function () {
+//     $('#plusAdd').trigger('focus')
+//   })$(document).ready(function() {
+    
+
     ///test for ajax/api call
     $("#searchBtn").on("click", function(event) {
         event.preventDefault();
@@ -49,41 +42,93 @@ $(document).ready(function() {
             })
             .then(function(response) {
                 console.log(response);
+                var arrayOfMeals = response.meals;
+                // TODO: iterate over all of the results
+                
+                for (let i = 0; i < arrayOfMeals.length; i++) {
+                    const meal = arrayOfMeals[i];
+                    console.log('meals thing: ' + i );
+                    // meal title
+                    // console.log(meal.strMeal);
+
+                    // meal img
+                    // console.log(meal.strMealThumb);
+                    
+                    var mealCard = createCard(meal);
+                    console.log('here is mealCard', mealCard);
+                    alert('stopping point');
+                    $('.recipe-box').append(mealCard);
+                    
+                    
+                    
+                }
+                
+
             });
-        $("#searchInput").val("");
     });
 
-});
+
+
+    // ===================================================
+    // helper functions
+    // ===================================================
+    function createCard(meal) {
+        var mealTitle = meal.strMeal;
+        var mealImg = meal.strMealThumb;
+        var mealTagsArray = meal.strTags.split(',');
+
+        var parentCard = $('<div>').addClass('card mx-auto');
+        var cardBody = $('<div>').addClass('card-body');
+        var parentCardRow = $('<div>').addClass('row');
+        parentCard.append(cardBody);
+        cardBody.append(parentCardRow);
+
+        // === left side ===
+        var cardImgDiv = $('<div>').addClass('container img-box col-xs-12 col-md-3');
+        var cardImg = $('<img>').attr('src', mealImg).addClass('card-img');
+        cardImgDiv.append(cardImg);
+        parentCardRow.append(cardImgDiv);
+
+        // === right side ===
+        var titleDiv = $('<div>').addClass('container col-xs-12 col-md-9');
+        var titleH3 = $('<h3>').addClass('text-right').text(mealTitle);
+        var titleP = $('<p>').addClass('text-right').text('This elegant dish can be made in under 30mins. Feeds 4. #dinner');
+        titleDiv.append(titleH3);
+        titleDiv.append(titleP);
+        
+        var divRow = $('<div>').addClass('row');
+        titleDiv.append(divRow);
+        var tagBox = $('<div>').addClass('container tag-box');
+        divRow.append(tagBox);
+        for (let i = 0; i < mealTagsArray.length; i++) {
+            const element = mealTagsArray[i];
+            var spanTag = $('<span>').addClass('badge badge-pill badge-info').text(element);
+            tagBox.append(spanTag);
+        }
+        parentCardRow.append(titleDiv);
+
+        return parentCard;
+    }
+
+    function appendCard(elementClass, card) {
+        elementClass = '.' + elementClass;
+    }
+
+    function addSuccessMessage(elementClass) {
+        elementClass = '.' + elementClass;
+        var successMessage = $('<div>').addClass('alert alert-success')
+            .attr('role', 'alert')
+            .text('Yay! you saved something!');
+
+        $(elementClass).prepend(successMessage);
+
+        // removes message after time
+        setTimeout(function() {
+            $(elementClass).detach();
+        }, 4 * 1000);
+    }
 
 
 
-// $('#addNewRecipe').on('shown.bs.modal', function () {
-//     $('#plusAdd').trigger('focus')
-//   })$(document).ready(function() {
-
-// ===================================================
-// EVENT - save recipe
-// ===================================================
-$('#save-recipe-btn').on('click', function(event) {
-    console.log('here is event', event);
-    var title = $('#recipe-input').val();
-    console.log('here is title: ' + title);
-
-    // TODO: save form entry to database
-
-
-    // TODO: when the data is saved to database, add success message
-    var successMessage = $('<div>').addClass('alert alert-success')
-        .attr('role', 'alert')
-        .text('Yay! you saved something!');
-
-    $('.card-message').prepend(successMessage);
-
-    // removes message after 5 seconds  
-    setTimeout(function() {
-        $('.card-message').detach();
-
-    }, 4 * 1000);
-
-
+// end of document ready
 });
