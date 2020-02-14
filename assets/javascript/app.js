70270c2e4928fa1b39e467f0b777a7bebbd361eb
Brian Allen <brianallen2401@gmail.com>
2020-02-14 15:42:53 -0700

    corrected some minor spelling and spacing errors

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -75,15 +75,12 @@ $(document).ready(function () {
         addSuccessMessage('card-message');
     });
 
-<<<<<<< HEAD
-=======
     // ===================================================
     // EVENT - go back button
     // ===================================================
     $('.go-back-btn').on('click', function(event) {
         // TODO: handle the go back button
     });
->>>>>>> b96d394f6dded3e98fda45544db38be719b42657
 
     // ===================================================
     // EVENT - Search button
