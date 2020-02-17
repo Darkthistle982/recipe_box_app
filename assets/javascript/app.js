75a4f11bf9d7c77f0cb484800bf54fd79f62b78b
Brian Allen <brianallen2401@gmail.com>
2020-02-17 15:48:21 -0700

    move a line. Uncommented a line

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -229,7 +229,7 @@ $(document).ready(function () {
         // === right side ===
         var titleDiv = $('<div>').addClass('container col-xs-12 col-md-9');
         var titleH3 = $('<h3>').addClass('text-right text-break').text(mealTitle);
-        // var titleP = $('<p>').addClass('text-right').text('This elegant dish can be made in under 30mins. Feeds 4. #dinner');
+        var titleP = $('<p>').addClass('text-right').text('This elegant dish can be made in under 30mins. Feeds 4. #dinner');
         var titleP = $('<p>').addClass('text-right').text(truncatedDirections.substr(0, 65));
         titleDiv.append(titleH3);
         titleDiv.append(titleP);
